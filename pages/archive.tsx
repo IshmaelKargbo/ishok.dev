0308d4f6914b1945e734e1365d8617ed204c3e1c
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-05-24 07:27:26 +0000

    table update

--- pages/archive.tsx
+++ pages/archive.tsx
@@ -20,18 +20,18 @@ const Archive: NextPage = () => {
             />
         <Layout>
             <div className="mb-10">
-                <h1 className="text-head title-text">Archive</h1>
-                <h6 className="font-head -mt-6 text-sec">A big list of things I’ve worked on</h6>
+                <h1 className="text-head md:title-text text-4xl">Archive</h1>
+                <h6 className="font-head md:mt-6 text-sm md:text-base text-sec">A big list of things I’ve worked on</h6>
             </div>
             <div>
-                <table className="w-full archive">
+                <table className="w-full archive hidden md:block">
                     <thead>
                         <tr>
-                            <th className="font-semibold text-left w-16">Year</th>
-                            <th className="font-semibold text-left w-72">Title</th>
-                            <th className="font-semibold text-left w-36">Made at</th>
-                            <th className="font-semibold text-left">Built with</th>
-                            <th className="font-semibold text-left">Link</th>
+                            <th className="font-semibold w-16">Year</th>
+                            <th className="font-semibold w-72">Title</th>
+                            <th className="font-semibold w-36">Made at</th>
+                            <th className="font-semibold">Built with</th>
+                            <th className="font-semibold">Link</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -100,6 +100,77 @@ const Archive: NextPage = () => {
                             <td className="title">Integrating Algolia Search with WordPress Multisite</td>
                             <td className="company"><span>Upstatement</span></td>
                             <td className="tech"><span>Next.js · TypeScript · Redux Toolkit · Stripe · Algolia · Firebase Auth · Formik · Yup ·  Vercel</span></td>
+                            <td className="link">
+                                <div className="space-x-3 flex">
+                                    <a href="/" className="text-head hover:text-sec"><FiGithub /></a>
+                                    <a href="/" className="text-head hover:text-sec"><HiOutlineExternalLink /></a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className="w-full archive block md:hidden">
+                    <thead>
+                        <tr>
+                            <th className="font-semibold w-16">Year</th>
+                            <th className="font-semibold w-72">Title</th>
+                            <th className="font-semibold">Link</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="cursor-default">
+                            <td className="year overline">2021</td>
+                            <td className="title">Koala Health</td>
+                            <td className="link">
+                                <div className="space-x-3 flex">
+                                    <a href="/" className="text-head hover:text-sec"><FiGithub /></a>
+                                    <a href="/" className="text-head hover:text-sec"><HiOutlineExternalLink /></a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="cursor-default">
+                            <td className="year overline">2021</td>
+                            <td className="title">Koala Health</td>
+                            <td className="link">
+                                <div className="space-x-3 flex">
+                                    <a href="/" className="text-head hover:text-sec"><FiGithub /></a>
+                                    <a href="/" className="text-head hover:text-sec"><HiOutlineExternalLink /></a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="cursor-default">
+                            <td className="year overline">2021</td>
+                            <td className="title">Koala Health</td>
+                            <td className="link">
+                                <div className="space-x-3 flex">
+                                    <a href="/" className="text-head hover:text-sec"><FiGithub /></a>
+                                    <a href="/" className="text-head hover:text-sec"><HiOutlineExternalLink /></a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="cursor-default">
+                            <td className="year overline">2021</td>
+                            <td className="title">Koala Health</td>
+                            <td className="link">
+                                <div className="space-x-3 flex">
+                                    <a href="/" className="text-head hover:text-sec"><FiGithub /></a>
+                                    <a href="/" className="text-head hover:text-sec"><HiOutlineExternalLink /></a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="cursor-default">
+                            <td className="year overline">2021</td>
+                            <td className="title">Koala Health</td>
+                            <td className="link">
+                                <div className="space-x-3 flex">
+                                    <a href="/" className="text-head hover:text-sec"><FiGithub /></a>
+                                    <a href="/" className="text-head hover:text-sec"><HiOutlineExternalLink /></a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr className="cursor-default">
+                            <td className="year overline">2021</td>
+                            <td className="title">Integrating Algolia Search with WordPress Multisite</td>
                             <td className="link">
                                 <div className="space-x-3 flex">
                                     <a href="/" className="text-head hover:text-sec"><FiGithub /></a>
