32123d154a14ba6df723f8a7a9018c36793b4077
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-01-16 20:13:12 +0000

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,8 @@ class MyDocument extends Document {
                 <Head>
                     <link href="https://fonts.cdnfonts.com/css/calibre" rel="stylesheet" />
                     <link href="https://fonts.cdnfonts.com/css/sf-mono" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet" />
+                    <meta charSet="utf-8" />
                 </Head>
                 <body>
                     <Main />
