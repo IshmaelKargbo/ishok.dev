4749374a5ce2c62faf7c8224d35fb0357f1f3be9
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-01-16 20:13:23 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  purge: ['./components/**/*.tsx', './pages/**/*.tsx','./public/**/*.html'],
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['./components/**/*.tsx', './pages/**/*.tsx', './public/**/*.html'],
+    content: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography')
+    ],
 }
